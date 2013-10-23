c05897484c0a91ee26084ff699bdfe328460d79d
Mike D <mike@buzzvertical.com>
2013-10-22 20:16:20 -0500

    Removed alerts and prompts

--- game.js
+++ game.js
@@ -2,45 +2,49 @@ $(document).ready(function() {
   //Create a variable to store a random number
   var Answer = Math.floor((Math.random()*100)+1);
   var Guess = getGuess();
-   var vGuess = validateGuess (Guess);
-  checkGuess (Guess, Answer);
+   //var vGuess = validateGuess (Guess);
+  //checkGuess (Guess, Answer);
+  $("button").click(function(){
+      alert("button was clicked");
+    });
+
 
   //Create a function that when called will grab the number from the user input
-  //After creating the function and grabbing the users input check to see if it is a number. 
+  //After creating the function and grabbing the users input check to see if it is a number.
     //Ensure it is not a letter, special character, empty, or spaces
-  
+
   //Check to see if users guess matches random number
   //Check to see if users guess is higher than random number
   //Check to see if users guess is lower than random number
-  
-  
+
+
   //Add a event listener to the submit button that calls the function to get users number
-  
-  
+
+
 });
 
 function getGuess () {
-	return prompt("Your Guess?");
-
+	//return prompt("Your Guess?");
+  return $("#userguess");
 }
 
 function checkGuess (number, target) {
 	if (number == target) {
-		alert ("You Got It!!");
+		return "You Got It!!";
 	}else if (number < target) {
-		alert ("Too Low!");
+		return "Too Low!";
 	} else {
-		alert ("Too High!");
+		return "Too High!";
 }
 }
-function validateGuess (userGuess) {
-	var isNan = isNaN(parseInt(userGuess));
-if (isNan)
-	{ alert (isNan)
-	  return prompt ("You must use a Number");
-	}else 
-  {
-		return userGuess;
-  }
-}
+//function validateGuess (userGuess) {
+	//var isNan = isNaN(parseInt(userGuess));
+//if (isNan)
+	//{ alert (isNan)
+	  //return prompt ("You must use a Number");
+	//}else
+  //{
+		//return userGuess;
+  //}
+//}
 
